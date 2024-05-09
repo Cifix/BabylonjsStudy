0332a1bb0081381bc6e95a865311192740d8ef3b
Cifix <66165076+Cifix@users.noreply.github.com>
2024-05-10 00:48:27 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -8,39 +8,8 @@ const createScene = function()
 {
   const scene = new BABYLON.Scene(engine);
 
-  scene.createDefaultCameraOrLight(true,false,true);//기본 조명 설치
-  /*
-  const box = new BABYLON.MeshBuilder.CreateBox('myBox',
-    {
-      size:0.1,
-      width:2,
-      height:0.05,
-      depth:0.5,
-      faceColors:[
-        new BABYLON.Color4(1,0,0,1),
-        BABYLON.Color3.Green()
-      ]
-    }
-  );//box 생성
-
+  scene.createDefaultCameraOrLight(true,false,true);
  
- const sphere = new BABYLON.MeshBuilder.CreateSphere('mySphere', 
- {segments: 50,
-  diameter:0.3
-
- }, scene);
-
-
-const ground = new BABYLON.MeshBuilder.CreateGround('', {
-  height:10,
-  width:10,
-  subdivisions:30
-});
-
-ground.material = new BABYLON.StandardMaterial();
-ground.material.wireframe = true;
-*/
-
 const groundFromHM = new BABYLON.MeshBuilder.CreateGroundFromHeightMap('','/Asset/Images/heightmap.png',
   {
     height:10,
@@ -48,7 +17,6 @@ const groundFromHM = new BABYLON.MeshBuilder.CreateGroundFromHeightMap('','/Asse
     subdivisions:100
   }
 );
-
 
 return scene;
 }
@@ -61,7 +29,7 @@ engine.runRenderLoop(function ()
 }
 );
 
-window.addEventListener('resize', function()//화면에 맞게 사이즈 조정
+window.addEventListener('resize', function()
 {
 engine.resize();
 }
